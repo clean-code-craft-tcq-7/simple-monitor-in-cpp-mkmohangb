27dac1f1dc1b3e8d99a7257675b5133ac097867c
Manoj Mohan <haimanojkumar@gmail.com>
2025-08-19 11:20:47 +0530

    add tests for legacy code

--- test-monitor.cpp
+++ test-monitor.cpp
@@ -3,5 +3,32 @@
 
 TEST(Monitor, NotOkWhenAnyVitalIsOffRange) {
   ASSERT_FALSE(vitalsOk(99, 102, 70));
-  ASSERT_TRUE(vitalsOk(98.1, 70, 98));
+}
+
+TEST(Monitor, OkWhenAllVitalsAreInRange) {
+  ASSERT_TRUE(vitalsOk(98, 75, 95));
+}
+
+TEST(Monitor, TemperatureCritical) {
+  ASSERT_FALSE(vitalsOk(103, 75, 95));
+}
+
+TEST(Monitor, PulseRateCritical) {
+  ASSERT_FALSE(vitalsOk(98, 55, 95));
+}
+
+TEST(Monitor, Spo2Critical) {
+  ASSERT_FALSE(vitalsOk(98, 75, 85));
+}
+
+TEST(Monitor, TemperatureOk) {
+  ASSERT_TRUE(isTemperatureOk(98));
+}
+
+TEST(Monitor, pulseRateOk) {
+  ASSERT_TRUE(isPulseRateOk(75));
+}
+
+TEST(Monitor, Spo2Ok) {
+  ASSERT_TRUE(isSpo2Ok(95));
 }
